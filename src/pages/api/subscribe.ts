37241c8904da002d9814658358387268b105b01a
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-23 16:42:13 -0300

    Change stripeCustomer.id to customerId in stripeCheckoutSession

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -7,6 +7,9 @@ import { stripe } from "../../services/stripe";
 type User = {
   ref: {
     id: string
+  },
+  data : {
+    stripe_customer_id: string
   }
 }
 
@@ -23,25 +26,29 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
       )
     )
 
-    const customerId = user.data.stripe_customer_id;
+    let customerId = user.data.stripe_customer_id;
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email
-    });
-    
-    await fauna.query(
-      q.Update(
-        q.Ref(q.Collection('users'), user.ref.id),
-        {
-          data: {
-            stripe_customer_id:  stripeCustomer.id
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email
+      });
+      
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id:  stripeCustomer.id
+            }
           }
-        }
-      )
-    );
+        )
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
