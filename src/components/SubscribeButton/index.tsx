229451e84516ca07b8ceea5823e75a42a72a0277
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-25 12:25:43 -0300

    Add validation activeSubscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,9 @@
 import { signIn, useSession } from "next-auth/client";
+import { useRouter } from "next/dist/client/router";
+
 import { api } from "../../services/api";
 import { getStripeJs } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -9,10 +12,16 @@ type SubscribeButtonProps = {
 
 export function SubscribeButton(props: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
