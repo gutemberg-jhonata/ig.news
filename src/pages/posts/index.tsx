3de95f94005c2bfa4737944eff218f47629535fb
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-24 21:30:10 -0300

    Add key property in posts list

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -28,7 +28,7 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a>
+            <a key={post.slug}>
               <time>{post.updatedAt}</time>
               <strong>{post.title}</strong>
               <p>{post.excerpt}</p>
